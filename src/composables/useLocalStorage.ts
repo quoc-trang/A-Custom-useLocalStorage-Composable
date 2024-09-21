a74d0f9da4b7093be83a7d8f7a9e67142de6a9f9
trang <trang.lq@raksul.com>
2024-09-21 22:03:42 +0700

    feat: useLocalStorage

--- src/composables/useLocalStorage.ts
+++ src/composables/useLocalStorage.ts
@@ -1,3 +1,22 @@
-export function useLocalStorage(key, defaultValue) {
-  return defaultValue;
+import { onUnmounted, ref, toRef, watch, watchEffect, type Ref } from "vue";
+
+export function useLocalStorage(key: string, defaultValue: string) {
+  const storage = ref(localStorage.getItem(key) ?? defaultValue);
+
+  watchEffect(() => {
+    localStorage.setItem(key, storage.value);
+  });
+
+  window.addEventListener("storage", syncChange);
+
+  function syncChange(event: StorageEvent) {
+    if (event.key) {
+      storage.value = event.newValue ?? "";
+    }
+  }
+
+  onUnmounted(() => {
+    window.removeEventListener("storage", syncChange);
+  });
+  return storage;
 }
